717517f17f91a9974b8a8979b444f7de664feab2
Scorhenao <samcorhenao@gmail.com>
2025-01-27 17:04:27 -0500

    feat: add a redirect to the login if the register is success

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,11 +1,14 @@
+import {useNavigation} from '@react-navigation/native';
 import {UserRegisterRequest} from '../../core/domain/request/UserRegisterRequest';
 import {userImplementation} from '../../core/infrastructure/implementations/UserImplementation';
 
 export const useUser = () => {
+  const navigation = useNavigation();
   const registerUser = async (data: UserRegisterRequest) => {
     try {
       const response = await userImplementation.registerUser(data);
       console.log('User registered successfully:', response.data);
+      navigation.navigate('Login');
       return response;
     } catch (error: any) {
       console.error(
