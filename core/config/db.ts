0d2799d0e7adedbb1092971aca21d2c22806b521
Scorhenao <samcorhenao@gmail.com>
2025-02-03 17:49:38 -0500

    refactor: add logs to see if the db is launched correctly

--- core/config/db.ts
+++ core/config/db.ts
@@ -1,6 +1,14 @@
 import SQLite from 'react-native-sqlite-storage';
 
-export const db = SQLite.openDatabase({
-  name: 'Habits.db',
-  location: 'default',
-});
+export const db = SQLite.openDatabase(
+  {
+    name: 'Habits.db',
+    location: 'default',
+  },
+  () => {
+    console.log('Database opened successfully');
+  },
+  error => {
+    console.error('Error opening database: ', error);
+  },
+);
